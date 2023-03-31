52c1c16c3406b57912e81698d0b991197547f3b7
Jadrian Abramovitch <jabramo1998@gmail.com>
2023-03-30 21:36:19 -0400

    fixing TS errors now that I can see errors

--- src/pages/ticTacToe/[gameId].tsx
+++ src/pages/ticTacToe/[gameId].tsx
@@ -1,5 +1,5 @@
 import { type NextPage } from 'next';
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useUserIp } from '../../utils/useUserIp';
@@ -7,7 +7,9 @@ import { useUserIp } from '../../utils/useUserIp';
 const TOTAL_MOVES_PLAYER_O = 5;
 const TOTAL_MOVES_PLAYER_X = 4;
 
-const ticTacToe: NextPage = () => {
+type CellState = 1 | 2 | typeof NaN;
+
+const TicTacToe: NextPage = () => {
     const ip = useUserIp();
     console.log('ip: ', ip);
     const router = useRouter();
@@ -16,43 +18,40 @@ const ticTacToe: NextPage = () => {
     console.log({ gameId });
 
     const getBoard = () => {
-        const gameBoard: number[][] = [
-            [NaN, NaN, NaN],
-            [NaN, NaN, NaN],
-            [NaN, NaN, NaN],
-        ];
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                gameBoard[i][j] = (
-                    <button
-                        onClick={() => handleClick(i, j)}
-                        key={i.toString() + '-' + j.toString()}
-                        className="m-4 h-32 w-32 bg-sky-500"
-                    >
-                        <h2 className="">{getSymbol(i, j)}</h2>
-                    </button>
-                );
-            }
-        }
-        return gameBoard;
+        const gameBoard = [0,1,2].map((i) => {
+            return([0,1,2].map((j) => ( 
+                <button
+                    onClick={() => handleClick(i, j)}
+                    key={i.toString() + '-' + j.toString()}
+                    className="m-4 h-32 w-32 bg-sky-500"
+                >
+                    <h2 className="">{mapper[getBoardValue(i, j)]}</h2>
+                </button>
+           ))); 
+        });
+        return gameBoard 
     };
     const [turn, setTurn] = useState(1); // 1 ->'O's, 2 -> x's
-    const [board, setBoard] = useState([
+    const [board, setBoard] = useState<number[][]>([
         [NaN, NaN, NaN],
         [NaN, NaN, NaN],
         [NaN, NaN, NaN],
     ]);
     const [winner, setWinner] = useState(NaN);
 
-    const mapper = {
+    const mapper = (boardValue: CellState) => {
+        const map = {
         NaN: '?',
         1: 'O',
-        2: 'X',
+        2: 'X',};
+        return map[boardValue];
     };
-
-    const getSymbol = (i: number, j: number) => {
-        return mapper[board[i][j]];
-    };
+    
+    const getBoardValue = (i: number, j: number): CellState => {
+        const row = board[i];
+        if (row === undefined) throw new Error();
+        return row[j] as CellState;
+    }
 
     const handleClick = (i: number, j: number) => {
         let oldBoard = board;
@@ -121,4 +120,4 @@ const ticTacToe: NextPage = () => {
     );
 };
 
-export default ticTacToe;
+export default TicTacToe;
