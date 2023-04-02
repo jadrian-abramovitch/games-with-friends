f656aee66bc1eb1305f50696b3b49b7f95d29fc2
Jadrian Abramovitch <jabramo1998@gmail.com>
2023-04-02 16:25:25 -0400

    fixing TS issues

--- src/pages/ticTacToe/[gameId].tsx
+++ src/pages/ticTacToe/[gameId].tsx
@@ -1,5 +1,5 @@
 import { type NextPage } from 'next';
-import { ReactNode, useState } from 'react';
+import { useState } from 'react';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useUserIp } from '../../utils/useUserIp';
@@ -25,13 +25,13 @@ const TicTacToe: NextPage = () => {
                     key={i.toString() + '-' + j.toString()}
                     className="m-4 h-32 w-32 bg-sky-500"
                 >
-                    <h2 className="">{mapper[getBoardValue(i, j)]}</h2>
+                    <h2 className="">{map[getBoardValue(i, j)]}</h2>
                 </button>
            ))); 
         });
         return gameBoard 
     };
-    const [turn, setTurn] = useState(1); // 1 ->'O's, 2 -> x's
+    const [turn, setTurn] = useState<number>(1); // 1 ->'O's, 2 -> x's
     const [board, setBoard] = useState<number[][]>([
         [NaN, NaN, NaN],
         [NaN, NaN, NaN],
@@ -39,12 +39,10 @@ const TicTacToe: NextPage = () => {
     ]);
     const [winner, setWinner] = useState(NaN);
 
-    const mapper = (boardValue: CellState) => {
-        const map = {
+    const map: { [key: CellState]: string } = {
         NaN: '?',
         1: 'O',
-        2: 'X',};
-        return map[boardValue];
+        2: 'X',
     };
     
     const getBoardValue = (i: number, j: number): CellState => {
@@ -54,44 +52,41 @@ const TicTacToe: NextPage = () => {
     }
 
     const handleClick = (i: number, j: number) => {
-        let oldBoard = board;
-        oldBoard[i][j] = turn;
+        const oldBoard = board;
+        const rowToUpdate = oldBoard[i];
+        if (rowToUpdate === undefined) throw new Error('Board is empty');
+        rowToUpdate[j] = turn;
+        oldBoard[i] = rowToUpdate;
         setBoard(oldBoard);
         setTurn(3 - turn); // flips between 2 and 1
-    };
-
-    const sendWinMessage = () => {
-        setWinner(turn);
-        window.alert(`Player ${turn} wins!`);
     };
 
     useEffect(() => {
         let sum = 0;
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                sum += board[i][j];
+                sum += getBoardValue(i,j);
             }
         }
         for (let i = 0; i < 3; i++) {
             // horizontal win
-            if ((board[i][0] + board[i][1] + board[i][2]) % 3 === 0) {
-                return sendWinMessage();
+            if ((getBoardValue(i, 0) + getBoardValue(i, 1) + getBoardValue(i, 2)) % 3 === 0) {
+                return setWinner(turn);
             }
             // vertical win
-            if ((board[0][i] + board[1][i] + board[2][i]) % 3 === 0) {
-                return sendWinMessage();
+            if ((getBoardValue(0, i) + getBoardValue(1, i) + getBoardValue(2, i)) % 3 === 0) {
+                return setWinner(turn);
             }
         }
         //diagonal wins
-        if ((board[0][0] + board[1][1] + board[2][2]) % 3 === 0) {
-            return sendWinMessage();
+        if ((getBoardValue(0, 0) + getBoardValue(1, 1) + getBoardValue(2, 2)) % 3 === 0) {
+            return setWinner(turn); 
         }
-        if ((board[0][2] + board[1][1] + board[2][0]) % 3 === 0) {
-            return sendWinMessage();
+        if ((getBoardValue(0, 2) + getBoardValue(1, 1) + getBoardValue(2, 0)) % 3 === 0) {
+            return setWinner(turn); 
         }
         if (sum === 1 * TOTAL_MOVES_PLAYER_O + 2 * TOTAL_MOVES_PLAYER_X) {
             setWinner(0);
-            window.alert('draw');
         }
     }, [JSON.stringify(board)]);
     return (
@@ -104,7 +99,7 @@ const TicTacToe: NextPage = () => {
                     ))}
                 {!isNaN(winner) && winner !== 0 && (
                     <h2 className="text-center text-4xl">
-                        Game Over, player {turn} wins!
+                        Game Over, player {winner} wins!
                     </h2>
                 )}
                 {!isNaN(winner) && winner === 0 && (
@@ -112,7 +107,7 @@ const TicTacToe: NextPage = () => {
                 )}
                 {isNaN(winner) && (
                     <h2 className="text-center text-4xl">
-                        Next Turn: {mapper[turn]}
+                        Next Turn: {map[turn]}
                     </h2>
                 )}
             </div>
