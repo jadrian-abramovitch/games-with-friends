370f683e5094d39328590250a14ade785de7f49f
Jadrian Abramovitch <jabramo1998@gmail.com>
2023-07-30 15:41:18 -0400

    I think it all just works now?

--- src/pages/ticTacToe/[gameId].tsx
+++ src/pages/ticTacToe/[gameId].tsx
@@ -7,120 +7,68 @@ import { useRouter } from 'next/router';
 const TOTAL_MOVES_PLAYER_O = 5;
 const TOTAL_MOVES_PLAYER_X = 4;
 
+const playerCookieExists = () => {
+    if (typeof window !== 'undefined') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            if (cookies[i]?.includes('ticTacToe')) {
+                return cookies[i];
+            }
+        }
+    }
+    return false;
+};
+const setCookie = (name: string, value: string, days?: number): void => {
+    if (typeof window !== 'undefined') {
+        let expires = '';
+        if (days) {
+            const date = new Date();
+            date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+            expires = '; expires=' + date.toUTCString();
+        }
+        document.cookie =
+            'ticTacToe' + name + '=' + value + expires + '; path=/';
+    }
+};
+
+const getCookie = (name: string): string | null => {
+    if (typeof window !== 'undefined') {
+        const nameEQ = name + '=';
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            console.log('cookie: ', c);
+            if (c.indexOf(nameEQ) == 0) {
+                console.log(
+                    'if statement',
+                    c.substring(nameEQ.length, c.length)
+                );
+                return c.substring(nameEQ.length, c.length);
+            }
+        }
+    }
+    return null;
+};
 type CellState = 1 | 2 | typeof NaN;
-const myRandomCookie = Math.random().toString();
+const map: { [key: CellState]: string } = {
+    NaN: '?',
+    1: 'O',
+    2: 'X',
+};
+
+const mapDbToBoard: { [key: string]: CellState } = {
+    '?': NaN,
+    O: 1,
+    X: 2,
+};
+
+const randomNumberString = Math.random().toString();
 
 const TicTacToe: NextPage = () => {
     const router = useRouter();
     const gameId = Number(router.query.gameId);
 
-    const registerToGame = api.ticTacToe.joinGame.useMutation();
-
-    const map: { [key: CellState]: string } = {
-        NaN: '?',
-        1: 'O',
-        2: 'X',
-    };
-
-    const mapDbToBoard: { [key: string]: CellState } = {
-        '?': NaN,
-        O: 1,
-        X: 2,
-    };
-
-    function transformBoard(stringBoard: string) {
-        const newBoard: number[][] = [0, 1, 2].map(() =>
-            [0, 1, 2].map(() => NaN)
-        );
-        let stringIndex = 0;
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                const temp: string = stringBoard[stringIndex] || '?';
-                newBoard[i][j] = mapDbToBoard[temp] || NaN;
-                stringIndex++;
-            }
-        }
-        return newBoard;
-    }
-
-    function setCookie(name: string, value: string, days?: number): void {
-        if (typeof window !== 'undefined') {
-            let expires = '';
-            if (days) {
-                const date = new Date();
-                date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-                expires = '; expires=' + date.toUTCString();
-            }
-            document.cookie = name + '=' + value + expires + '; path=/';
-        }
-    }
-
-    function getCookie(name: string): string | null {
-        if (typeof window !== 'undefined') {
-            const nameEQ = name + '=';
-            const ca = document.cookie.split(';');
-            for (let i = 0; i < ca.length; i++) {
-                let c = ca[i];
-                while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-                if (c.indexOf(nameEQ) == 0)
-                    return c.substring(nameEQ.length, c.length);
-            }
-        }
-        return null;
-    }
-
-    const sendMove = api.ticTacToe.playSquare.useMutation();
-    const playerCookie = getCookie(myRandomCookie);
-    if (!playerCookie) {
-        setCookie(myRandomCookie, myRandomCookie, 5);
-    } else if (gameId && registerToGame.isIdle) {
-        registerToGame.mutate({
-            gameId: Number(gameId),
-            playerId: playerCookie,
-        });
-    }
-
-    const boardIsEmpty = () => {
-        console.log(board.flat().reduce((total, current) => total + current));
-        return board.flat().reduce((total, current) => total + current) < 1;
-    };
-
-    const getBoard = (board: number[][]) => {
-        return board.map((row, i: number) => {
-            return row.map((cell, j: number) => {
-                return (
-                    <button
-                        disabled={!isNaN(cell)}
-                        onClick={() => handleClick(i, j)}
-                        key={i.toString() + '-' + j.toString()}
-                        className="m-4 h-32 w-32 bg-sky-500"
-                    >
-                        <h2 className="">
-                            {isNaN(cell) ? '?' : map[getBoardValue(i, j)]}
-                        </h2>
-                    </button>
-                );
-            });
-        });
-        /*
-        if (!boardIsEmpty()) {
-            //window.alert('not empty');
-            const gameBoard = [0,1,2].map((i) => {
-                return([0,1,2].map((j) => ( 
-                    <button
-                        onClick={() => handleClick(i, j)}
-                        key={i.toString() + '-' + j.toString()}
-                        className="m-4 h-32 w-32 bg-sky-500"
-                    >
-                        <h2 className="">{map[getBoardValue(i, j)]}</h2>
-                    </button>
-                ))); 
-            });
-        return gameBoard
-        } else {
-            return board;
-        }*/
-    };
     const [turn, setTurn] = useState<number>(1); // 1 ->'O's, 2 -> x's
     const [board, setBoard] = useState<number[][]>([
         [NaN, NaN, NaN],
@@ -128,8 +76,73 @@ const TicTacToe: NextPage = () => {
         [NaN, NaN, NaN],
     ]);
     const [winner, setWinner] = useState(NaN);
+    const [playerCookie, setPlayerCookie] = useState<string>();
+    useEffect(
+        () => console.log('cookie changed fam: ', playerCookie),
+        [playerCookie]
+    );
 
-    const getBoardValue = (i: number, j: number): CellState => {
+    const registerToGame = api.ticTacToe.joinGame.useMutation();
+    const sendMove = api.ticTacToe.playSquare.useMutation({
+        onSuccess: () => gameStateQuery.refetch(),
+    });
+    const gameStateQuery = api.ticTacToe.getBoard.useQuery(
+        { gameId: gameId },
+        {
+            onSuccess: (data) => setBoard(transformBoardToArray(data?.board)),
+        }
+    );
+
+    function transformBoardToArray(stringBoard: string) {
+        const newBoard: number[][] = [0, 1, 2].map(() =>
+            [0, 1, 2].map(() => NaN)
+        );
+        let stringIndex = 0;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const temp: string = stringBoard[stringIndex] || '?';
+                newBoard[i]![j] = mapDbToBoard[temp] || NaN;
+                stringIndex++;
+            }
+        }
+        return newBoard;
+    }
+
+    const currentPlayerCookie = playerCookieExists();
+    console.log('cookie logslkfanj: ', currentPlayerCookie, playerCookie);
+    if (currentPlayerCookie && currentPlayerCookie !== playerCookie) {
+        console.log('if statement is true');
+        setPlayerCookie(currentPlayerCookie);
+    } else if (!currentPlayerCookie) {
+        setCookie(randomNumberString, '', 2);
+        // setPlayerCookie(randomNumberString);
+    }
+    if (playerCookie && gameId && registerToGame.isIdle) {
+        registerToGame.mutate({
+            gameId: Number(gameId),
+            playerId: playerCookie,
+        });
+    }
+
+    const boardStructure = (board: number[][]) => {
+        return board.map((row, i: number) => {
+            return row.map((cell, j: number) => {
+                return (
+                    <button
+                        disabled={!isNaN(cell)}
+                        onClick={async () => await handleClick(i, j)}
+                        key={i.toString() + '-' + j.toString()}
+                        className="m-4 h-32 w-32 bg-sky-500"
+                    >
+                        <h2 className="">
+                            {isNaN(cell) ? '?' : map[boardAt(i, j)]}
+                        </h2>
+                    </button>
+                );
+            });
+        });
+    };
+    const boardAt = (i: number, j: number): CellState => {
         const row = board[i];
         if (row === undefined) throw new Error();
         return row[j] as CellState;
@@ -137,68 +150,41 @@ const TicTacToe: NextPage = () => {
 
     const handleClick = async (i: number, j: number) => {
         if (typeof playerCookie === 'string' && typeof gameId === 'number') {
-            await sendMove.mutate({
+            sendMove.mutate({
                 gameId,
                 playerCookie,
                 xLocation: i,
                 yLocation: j,
             });
-            await gameStateQuery.refetch();
         }
         setTurn(3 - turn); // flips between 2 and 1
     };
-
-    const gameStateQuery = api.ticTacToe.getBoard.useQuery(
-        { gameId: gameId },
-        {
-            onSuccess: (data) => setBoard(transformBoard(data?.board)),
-        }
-    );
 
     useEffect(() => {
         let sum = 0;
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                sum += getBoardValue(i, j);
+                sum += boardAt(i, j);
             }
         }
         for (let i = 0; i < 3; i++) {
             // horizontal win
-            if (
-                (getBoardValue(i, 0) +
-                    getBoardValue(i, 1) +
-                    getBoardValue(i, 2)) %
-                    3 ===
-                0
-            ) {
+            if ((boardAt(i, 0) + boardAt(i, 1) + boardAt(i, 2)) % 3 === 0) {
                 return setWinner(turn);
             }
             // vertical win
-            if (
-                (getBoardValue(0, i) +
-                    getBoardValue(1, i) +
-                    getBoardValue(2, i)) %
-                    3 ===
-                0
-            ) {
+            if ((boardAt(0, i) + boardAt(1, i) + boardAt(2, i)) % 3 === 0) {
                 return setWinner(turn);
             }
         }
         //diagonal wins
-        if (
-            (getBoardValue(0, 0) + getBoardValue(1, 1) + getBoardValue(2, 2)) %
-                3 ===
-            0
-        ) {
+        if ((boardAt(0, 0) + boardAt(1, 1) + boardAt(2, 2)) % 3 === 0) {
             return setWinner(turn);
         }
-        if (
-            (getBoardValue(0, 2) + getBoardValue(1, 1) + getBoardValue(2, 0)) %
-                3 ===
-            0
-        ) {
+        if ((boardAt(0, 2) + boardAt(1, 1) + boardAt(2, 0)) % 3 === 0) {
             return setWinner(turn);
         }
+        // Draw -- all tiles played and no winner
         if (sum === 1 * TOTAL_MOVES_PLAYER_O + 2 * TOTAL_MOVES_PLAYER_X) {
             setWinner(0);
         }
@@ -208,7 +194,7 @@ const TicTacToe: NextPage = () => {
             <div>
                 <h1 className="text-center text-7xl">TicTacToe!</h1>
                 {isNaN(winner) &&
-                    getBoard(board).map((row, index: number) => (
+                    boardStructure(board).map((row, index: number) => (
                         <div key={index.toString()}>{row}</div>
                     ))}
                 {!isNaN(winner) && winner !== 0 && (
@@ -219,18 +205,6 @@ const TicTacToe: NextPage = () => {
                 {!isNaN(winner) && winner === 0 && (
                     <h2 className="text-center text-4xl">Game Over, Draw!</h2>
                 )}
-                {isNaN(winner) && gameStateQuery.isSuccess && (
-                    <h2 className="text-center text-4xl">
-                        <h2>
-                            {JSON.stringify(
-                                transformBoard(gameStateQuery.data.board)
-                            )}
-                        </h2>
-                    </h2>
-                )}
-                <button onClick={async () => await gameStateQuery.refetch()}>
-                    Refresh Board
-                </button>
             </div>
         </div>
     );
--- src/server/api/routers/ticTacToe.ts
+++ src/server/api/routers/ticTacToe.ts
@@ -1,51 +1,97 @@
-import { z } from "zod";
+import { z } from 'zod';
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
 
 export const ticTacToeRouter = createTRPCRouter({
-  startNewGame: publicProcedure
-    .mutation(({ ctx }) => {
-      return ctx.prisma.game.create({data: {}});
+    startNewGame: publicProcedure.mutation(({ ctx }) => {
+        return ctx.prisma.game.create({ data: {} });
     }),
-  joinGame: publicProcedure
-    .input(z.object({gameId: z.number().int(), playerId: z.string()}))
-    .mutation( async ({ctx, input}) => {
-    const game = await ctx.prisma.game.findUniqueOrThrow({where: {gameId: input.gameId}});
-    if (!game.player1) {
-      return ctx.prisma.game.update({where: {gameId: input.gameId}, data: {player1: input.playerId}});
-    } else if (game.player1) {
-      return ctx.prisma.game.update({where: {gameId: input.gameId}, data: {player2: input.playerId}});
-    }
-  }),
-  getBoard: publicProcedure
-    .input(z.object({ gameId: z.number().int() }))
-    .query( ({ctx, input }) => {
-      console.log('sdfkljasdf');
-    return (ctx.prisma.game.findUniqueOrThrow({ where: {gameId: input.gameId}, select: {board: true} })); 
-  }),
-    
-  playSquare: publicProcedure
-  .input(z.object({playerCookie: z.string(), gameId: z.number().int(), xLocation: z.number().int(), yLocation: z.number().int()}))
-  .mutation( async ({ ctx, input: {playerCookie, xLocation, yLocation, gameId}}) => {
-    //needs to be in a transaction
-    const game = await ctx.prisma.game.findUniqueOrThrow({where: {gameId: gameId}});
-    const index = xLocation*3 + yLocation;
-   
-    //check player actually exists
-    let playerNumber: number;
-    if (game.player1 === playerCookie) { playerNumber = 1 }
-    else if (game.player2 === playerCookie) { playerNumber = 2 }
-    else { throw Error('Player does not exist in this game') }
-   
-    //check turn
-    if (playerNumber !== game.currentTurn) {throw Error('Other player\'s turn')}
+    joinGame: publicProcedure
+        .input(z.object({ gameId: z.number().int(), playerId: z.string() }))
+        .mutation(async ({ ctx, input }) => {
+            const game = await ctx.prisma.game.findUniqueOrThrow({
+                where: { gameId: input.gameId },
+            });
+            if (
+                input.playerId === game.player1 ||
+                input.playerId === game.player2
+            ) {
+                return;
+            }
+            if (!game.player1) {
+                return ctx.prisma.game.update({
+                    where: { gameId: input.gameId },
+                    data: { player1: input.playerId },
+                });
+            } else if (game.player1) {
+                return ctx.prisma.game.update({
+                    where: { gameId: input.gameId },
+                    data: { player2: input.playerId },
+                });
+            }
+        }),
+    getBoard: publicProcedure
+        .input(z.object({ gameId: z.number().int() }))
+        .query(({ ctx, input }) => {
+            console.log('sdfkljasdf');
+            return ctx.prisma.game.findUniqueOrThrow({
+                where: { gameId: input.gameId },
+                select: { board: true },
+            });
+        }),
 
-    // check if square is empty
-    if (!game.board[index]) { throw Error('Square is not empty'); }
-    
-    const playerSymbol = playerNumber === 1 ? 'X' : 'O';
-    const newBoard = game.board.slice(0, index) + playerSymbol + game.board.slice(index + 1);
-    return ctx.prisma.game.update({where: {gameId: game.gameId}, data: {board: newBoard, currentTurn: 3 - game.currentTurn}});
-  })
+    playSquare: publicProcedure
+        .input(
+            z.object({
+                playerCookie: z.string(),
+                gameId: z.number().int(),
+                xLocation: z.number().int(),
+                yLocation: z.number().int(),
+            })
+        )
+        .mutation(
+            async ({
+                ctx,
+                input: { playerCookie, xLocation, yLocation, gameId },
+            }) => {
+                //needs to be in a transaction
+                const game = await ctx.prisma.game.findUniqueOrThrow({
+                    where: { gameId: gameId },
+                });
+                const index = xLocation * 3 + yLocation;
+
+                //check player actually exists
+                let playerNumber: number;
+                if (game.player1 === playerCookie) {
+                    playerNumber = 1;
+                } else if (game.player2 === playerCookie) {
+                    playerNumber = 2;
+                } else {
+                    throw Error('Player does not exist in this game');
+                }
+
+                //check turn
+                if (playerNumber !== game.currentTurn) {
+                    throw Error("Other player's turn");
+                }
+
+                // check if square is empty
+                if (!game.board[index]) {
+                    throw Error('Square is not empty');
+                }
+
+                const playerSymbol = playerNumber === 1 ? 'X' : 'O';
+                const newBoard =
+                    game.board.slice(0, index) +
+                    playerSymbol +
+                    game.board.slice(index + 1);
+                return ctx.prisma.game.update({
+                    where: { gameId: game.gameId },
+                    data: {
+                        board: newBoard,
+                        currentTurn: 3 - game.currentTurn,
+                    },
+                });
+            }
+        ),
 });
-
